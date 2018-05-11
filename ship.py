97c7424e1475f148c125419ac6c19e09ad253776
Kwame paulin Saviour Blagogee <kwamepaulin.blagogee@stu.jefferson.kyschools.us>
2018-05-11 08:29:22 -0400

    Initial commit

---
+++ ship.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Ship():
+    def __init__(self, screen):
+        """Initalize the ship and set its starting position"""
+        self.screen = screen
+        self.image = pygame.image.load('image/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+
+def blitme(self):
+    """"Draw the ship at its current location"""
+    self.screen.blit(self.image, self.rect)
